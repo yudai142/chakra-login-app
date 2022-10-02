a1e9588d7838415ab4bc6549a209aa6064a27314
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-10-02 15:27:10 +0900

    フォーム作成

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,3 +1,49 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+  confirmationPassword: string;
+};
+
 export default function Signup() {
-  return <div>Signup page</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  return (
+    <div>
+      <form>
+        <div>
+          <label>Eメール</label>
+          <input type="email" placeholder="example@text.com" {...register("email", { required: true })}/>
+          {errors.email && (
+            <p>
+              Eメールは必須です
+            </p>
+          )}
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input type="password" {...register("password", { required: true })}/>
+          {errors.password && (
+            <p>
+              パスワードは必須です
+            </p>
+          )}
+        </div>
+        <div>
+          <label>パスワード確認</label>
+          <input type="password" {...register("confirmationPassword", { required: true })}/>
+          {errors.confirmationPassword && (
+            <p>
+              パスワード再入力は必須です
+            </p>
+          )}
+        </div>
+        <button type="submit">サインアップ</button>
+      </form>
+    </div>
+  )
 }
