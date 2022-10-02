80db2ccfcf75107a02c0458e169d5bb5388ebe20
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-10-02 17:19:40 +0900

    完成

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,6 +3,17 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useAuth, useUser } from "../hooks/firebase";
+import {
+  Box,
+  Button,
+  Flex,
+  FormLabel,
+  Heading,
+  Image,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import Link from "next/link";
 
 type Inputs = {
   email: string;
@@ -16,17 +27,16 @@ export default function Signup() {
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>();
+
   const auth = useAuth();
   const currentUser = useUser();
+  const [isProcessingSignup, setIsProcessingSignup] = useState(false);
   const router = useRouter();
   const signup = async (email: string, password: string) => {
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log("ユーザー登録成功")
+      setIsProcessingSignup(true);
+      await createUserWithEmailAndPassword(auth, email, password);
+      setIsProcessingSignup(false);
     } catch (e) {
       console.error(e);
     }
@@ -42,41 +52,103 @@ export default function Signup() {
       alert("パスワードが一致しません");
     }
   };
+
   useEffect(() => {
     if (currentUser) router.push("/");
   }, [currentUser, router]);
+
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Eメール</label>
-          <input type="email" placeholder="example@text.com" {...register("email", { required: true })}/>
-          {errors.email && (
-            <p>
-              Eメールは必須です
-            </p>
-          )}
-        </div>
-        <div>
-          <label>パスワード</label>
-          <input type="password" {...register("password", { required: true })}/>
-          {errors.password && (
-            <p>
-              パスワードは必須です
-            </p>
-          )}
-        </div>
-        <div>
-          <label>パスワード確認</label>
-          <input type="password" {...register("confirmationPassword", { required: true })}/>
-          {errors.confirmationPassword && (
-            <p>
-              パスワード再入力は必須です
-            </p>
-          )}
-        </div>
-        <button type="submit">サインアップ</button>
-      </form>
-    </div>
-  )
+    <Flex>
+      <Box
+        w="50%"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+      >
+        <Heading color="gray.800" mb="48px" textAlign="center" size="xl">
+          ようこそ
+        </Heading>
+        <Box
+          boxShadow="lg"
+          w="600px"
+          paddingY="120px"
+          paddingX="32px"
+          borderRadius="8px"
+          border="1px solid"
+          borderColor="gray.100"
+          m="0 auto"
+          display="flex"
+        >
+          <Box w="100%">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormLabel fontWeight="bold">Eメール</FormLabel>
+              {errors.email && (
+                <Text color="red.400" mb="8px">
+                  Eメールは必須です
+                </Text>
+              )}
+              <Input
+                type="email"
+                size="lg"
+                mb="8"
+                placeholder="example@test.com"
+                {...register("email", { required: true })}
+              />
+              <FormLabel fontWeight="bold">パスワード</FormLabel>
+              {errors.password && (
+                <Text color="red.400" mb="8px">
+                  パスワードは必須です
+                </Text>
+              )}
+              <Input
+                type="password"
+                {...register("password", { required: true })}
+                size="lg"
+                mb="8"
+              />
+              <FormLabel fontWeight="bold">パスワード再入力</FormLabel>
+              {errors.confirmationPassword && (
+                <Text color="red.400" mb="8px">
+                  パスワード再入力は必須です
+                </Text>
+              )}
+              <Input
+                type="password"
+                {...register("confirmationPassword", { required: true })}
+                size="lg"
+                mb="8"
+              />
+
+              <Flex flexDirection="column">
+                <Text mb="8" textAlign="center">
+                  アカウントをお持ちの方は
+                  <Link href="/login">こちら</Link>
+                </Text>
+                <Button
+                  type="submit"
+                  color="white"
+                  background="gray.800"
+                  size="lg"
+                  paddingX="80px"
+                  m="0 auto"
+                  isLoading={isProcessingSignup}
+                  _hover={{
+                    background: "gray.700",
+                  }}
+                >
+                  サインアップ
+                </Button>
+              </Flex>
+            </form>
+          </Box>
+        </Box>
+      </Box>
+      <Image
+        w="50%"
+        h="100vh"
+        alt="カバー画像"
+        src="https://images.unsplash.com/photo-1652554715588-60c932f66a0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+      />
+    </Flex>
+  );
 }
